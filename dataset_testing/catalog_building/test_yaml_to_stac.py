f21c9d4d0cc553356f76edc1b8fef75000d4cf3d
Florian Ziemen <florianziemen@users.noreply.github.com>
2024-11-22 15:56:13 +0100

    fix things for tests

--- dataset_testing/catalog_building/test_yaml_to_stac.py
+++ dataset_testing/catalog_building/test_yaml_to_stac.py
@@ -1,15 +1,15 @@
 import intake_to_stac
 from pystac import CatalogType
 import intake
-
+from pathlib import Path
 
 def test_cgen():
     cgen = intake_to_stac.catalog_generator()
-    cgen.parse_file("sample_dataset.yaml")
+    cgen.parse_file(Path(__file__).parent / Path("sample_dataset.yaml"))
     cgen.catalog.normalize_and_save(
         root_href="./", catalog_type=CatalogType.SELF_CONTAINED
     )
 
 
 def test_read():
-    intake.open_stac_catalog("catalog.json")["ngc4008"]["data_P1D_0"].to_dask()
+    intake.open_stac_catalog((Path(__file__).parent/Path("catalog.json")).as_posix())["ngc4008"]["data_P1D_0"].to_dask()
