014cb7956b3cd9f9709526a63d345abc89ab249b
Florian Ziemen <ziemen@dkrz.de>
2025-02-18 12:15:25 +0100

    Actually test if values in the dataset are sane.

--- python_envs/tests/test_eerie_cat.py
+++ python_envs/tests/test_eerie_cat.py
@@ -5,4 +5,7 @@ def test_eerie_cat():
     baseurl = "https://eerie.cloud.dkrz.de/datasets"
     dataset = "ifs-amip-tco1279.hist.v20240901.atmos.native.2D_24h"
     stac_item = intake.open_stac_item("/".join([baseurl, dataset, "stac"]))
-    stac_item["data"].to_dask()
+    ds = stac_item["data"].to_dask()
+    tmean = ds.mean2t.isel(time=2).mean().values
+    assert tmean > 280
+    assert tmean < 290
