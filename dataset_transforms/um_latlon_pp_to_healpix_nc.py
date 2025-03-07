1d600bc53018bf55fa52c103c38f907d90fc9d79
Mark Muetzelfeldt <markmuetz@gmail.com>
2025-03-07 12:19:05 +0000

    Handle pp files that contain multiple cubes.

--- dataset_transforms/um_latlon_pp_to_healpix_nc.py
+++ dataset_transforms/um_latlon_pp_to_healpix_nc.py
@@ -75,24 +75,32 @@ def hp_coarsen_with_weights(data, weights=None):
 class UMRegridder:
     """Regrid UM lat/lon .pp files to healpix .nc"""
 
-    def __init__(self, method='easygems_delaunay', zooms=range(11)[::-1], weights_path=WEIGHTS_PATH, tmpdir=TMPDIR):
+    VARNAMES_TO_PROCESS = {
+        'air_temperature',
+        'toa_outgoing_longwave_flux',
+    }
+
+    def __init__(self, method='easygems_delaunay', zooms=range(11)[::-1], attrs=None, weights_path=WEIGHTS_PATH, tmpdir=TMPDIR):
         """Initate a UM regridder for a particular method/zoom levels.
 
         Parameters:
             method (str) : regridding method [easygems_delaunay, earth2grid]
             zooms (list) : list of zoom levels (highest first)
         """
+        if attrs is None:
+            attrs = {}
         if method not in ['easygems_delaunay', 'earth2grid']:
             raise ValueError('method must be either easygems_delaunay or earth2grid')
         self.method = method
         self.zooms = list(zooms)
+        self.attrs = attrs
         self.max_zoom_level = zooms[0]
         self.weights_path = weights_path
         if method == 'easygems_delaunay':
             self.weights = xr.open_dataset(self.weights_path)
         self.tmpdir = Path(tmpdir)
 
-    def run(self, inpath, outpath_tpl, varname):
+    def run(self, inpath, outpath_tpl):
         """Do end-to-end regridding, including converting to tmp .nc file on scratch
         There will be one healpix .nc for every lat/lon .pp.
         NOTE: This assumes one variable per .pp file.
@@ -100,35 +108,50 @@ class UMRegridder:
         Parameters:
             inpath (str, Path) : path to input file
             outpath_tpl (str, Path) : path template (with {zoom}) to output file
-            varname (str) : name of variable to regrid
         """
-        print(f'Run regridding: {inpath}:{varname}')
+        print(f'Run regridding: {inpath}')
         print(f'-> {outpath_tpl} zooms={self.zooms}')
         inpath = Path(inpath)
         print('- convert .pp to .nc')
-        tmppath = self._pp_to_nc(inpath)
+        names_tmppaths = self._pp_to_nc(inpath)
         try:
-            print('- load .nc')
-            da = self._load_da(tmppath, varname)
-            print(f'- do regrid using {self.method}')
-            regridded_data, dim_shape, dim_ranges = self.regrid(da)
-            print(f'- coarsen and save')
-            self._coarsen_and_save(da, regridded_data, outpath_tpl, dim_shape, dim_ranges, varname)
+            for varname, tmppath in names_tmppaths:
+                msg = f'Process variable: {varname}'
+                print('=' * len(msg))
+                print(msg)
+                print('=' * len(msg))
+
+                print('- load .nc')
+                da = self._load_da(tmppath, varname)
+                print(f'- do regrid using {self.method}')
+                regridded_data, dim_shape, dim_ranges = self.regrid(da)
+                print(f'- coarsen and save')
+                self._coarsen_and_save(da, regridded_data, outpath_tpl, dim_shape, dim_ranges, varname)
         finally:
-            tmppath.unlink()
+            for _, tmppath in names_tmppaths:
+                tmppath.unlink()
 
     def _pp_to_nc(self, inpath):
         """Convert inpath (.pp) to .nc"""
-        tmppath = self.tmpdir.joinpath(*inpath.parts[1:]).with_suffix('.nc')
-        tmppath.parent.mkdir(exist_ok=True, parents=True)
 
         # Stop annoying error message.
         iris.FUTURE.save_split_attrs = True
         # For some reason, loading .pp then saving as .nc using iris, then reloading with xarray
         # is way faster.
-        cube = iris.load_cube(inpath)
-        iris.save(cube, tmppath)
-        return tmppath
+        cubes = iris.load(inpath)
+        names_tmppaths = []
+        print(f'  - found {len(cubes)} cube(s)')
+        print(f'    - {[c.name() for c in cubes]}')
+        for cube in cubes:
+            name = cube.name()
+            if name in self.VARNAMES_TO_PROCESS:
+                tmppath = self.tmpdir.joinpath(*inpath.parts[1:]).with_suffix(f'.{name}.nc')
+                tmppath.parent.mkdir(exist_ok=True, parents=True)
+                iris.save(cube, tmppath)
+                names_tmppaths.append((name, tmppath))
+        print(f'  - processing {len(names_tmppaths)} cube(s)')
+        print(f'    - {[n for n, p in names_tmppaths]}')
+        return names_tmppaths
 
     @staticmethod
     def _load_da(path, varname):
@@ -152,6 +175,7 @@ class UMRegridder:
         dim_ranges = [range(s) for s in dim_shape]
         ncell = 12 * 4 ** self.max_zoom_level
         regridded_data = np.zeros(dim_shape + [ncell])
+        print(f'  - {dict(dsout_tpl.dims)}')
         if self.method == 'easygems_delaunay':
             self._regrid_easygems_delaunay(da, dim_ranges, regridded_data)
         elif self.method == 'earth2grid':
@@ -170,7 +194,6 @@ class UMRegridder:
         # I'm not assuming these will be installed.
         import earth2grid
         import torch
-
 
         lat_lon_shape = (len(da.latitude), len(da.longitude))
         src = earth2grid.latlon.equiangular_lat_lon_grid(*lat_lon_shape)
@@ -194,8 +217,16 @@ class UMRegridder:
         """Produce the all zoom level data by successively coarsening and save."""
         dsout_tpl = xr.Dataset(coords=da.copy().drop_vars(['latitude', 'longitude']).coords, attrs=da.attrs)
         reduced_dims = [d for d in da.dims if d not in ['latitude', 'longitude']]
+
+        num_nans = np.isnan(da.values).sum()
+        print(f'  - {num_nans} NaNs in {varname}')
+        if num_nans:
+            coarsen = 'with_weights'
+        else:
+            coarsen = 'without_weights'
+
         for zoom in self.zooms:
-            outpath = Path(str(outpath_tpl).format(zoom=zoom))
+            outpath = Path(str(outpath_tpl).format(zoom=zoom, varname=varname))
             dsout = dsout_tpl.copy()
             if zoom != self.max_zoom_level:
                 coarse_regridded_data = np.zeros(dim_shape + [12 * 4 ** zoom])
@@ -207,8 +238,7 @@ class UMRegridder:
                                                coords={'cell': np.arange(regridded_data.shape[-1])})
             dsout.attrs['grid_mapping'] = 'healpix_nested'
             dsout.attrs['healpix_zoom'] = zoom
-            # dsout.attrs['input_file'] = str(inpath)
-            dsout.attrs['suite'] = 'u-cu087'
+            dsout.attrs.update(self.attrs)
 
             outpath.parent.mkdir(exist_ok=True, parents=True)
             print(f'  - {outpath} {zoom}')
@@ -235,21 +265,19 @@ def main():
     if len(sys.argv) == 3 and sys.argv[1] == 'toa_outgoing_longwave_flux':
         method = sys.argv[2]
         inpath = Path('/gws/nopw/j04/hrcm/cache/torau/Lorenzo_u-cu087/OLR/20200101T0000Z_pa000.pp')
-        outpath_tpl = f'/work/scratch-nopw2/mmuetz/wcrp_hackathon/OLR/20200101T0000Z_pa000.hpz{{zoom}}.{method}.nc'
-        varname = 'toa_outgoing_longwave_flux'
+        outpath_tpl = f'/work/scratch-nopw2/mmuetz/wcrp_hackathon/OLR/20200101T0000Z_pa000.{{varname}}.hpz{{zoom}}.{method}.nc'
     elif len(sys.argv) == 3 and sys.argv[1] == 'air_temperature':
         method = sys.argv[2]
         inpath = '/gws/nopw/j04/hrcm/hackathon/3D/pe_T/20200101T0000Z_pe000.pp'
-        outpath_tpl = f'/work/scratch-nopw2/mmuetz/wcrp_hackathon/pe_T/20200101T0000Z_pe000.hpz{{zoom}}.{method}.nc'
-        varname = 'air_temperature'
+        outpath_tpl = f'/work/scratch-nopw2/mmuetz/wcrp_hackathon/pe_T/20200101T0000Z_pe000.{{varname}}.hpz{{zoom}}.{method}.nc'
     else:
         inpath = sys.argv[1]
         outpath_tpl = sys.argv[2]
-        varname = sys.argv[3]
-        method = sys.argv[4]
+        method = sys.argv[3]
 
     um_regridder = UMRegridder(method)
-    um_regridder.run(inpath, outpath_tpl, varname)
+    um_regridder.run(inpath, outpath_tpl)
+
     return um_regridder
 
 
