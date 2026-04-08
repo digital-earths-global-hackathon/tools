d84854a82aa37c8ae549ae741ad914a5a3bbb67b
Florian Ziemen <ziemen@dkrz.de>
2026-04-08 18:40:57 +0200

    turn test into pytest

--- python_envs/tests/test_wwestac.py
+++ python_envs/tests/test_wwestac.py
@@ -7,14 +7,15 @@ import matplotlib.pyplot as plt
 import xpystac  # noqa: F401 – registers the "stac" engine with xarray, just needs to be there, not actually imported
 import intake
 
-catalog = pystac_client.Client.open("https://wwestac.cloud.dkrz.de/stac-fastapi-es/")
-item = catalog.get_collection("ngc4008").get_item("ngc4008_P1D_7")
-asset = item.assets["disk"]
-ds = xr.open_dataset(asset)
+def test_wwestac():
+    catalog = pystac_client.Client.open("https://wwestac.cloud.dkrz.de/stac-fastapi-es/")
+    item = catalog.get_collection("ngc4008").get_item("ngc4008_P1D_7")
+    asset = item.assets["disk"]
+    ds = xr.open_dataset(asset)
 
-cat = intake.open_catalog(
-    "https://digital-earths-global-hackathon.github.io/catalog/catalog.yaml"
-)
-ds2 = cat["online.icon_ngc4008"](zoom=7).to_dask()
+    cat = intake.open_catalog(
+        "https://digital-earths-global-hackathon.github.io/catalog/catalog.yaml"
+    )
+    ds2 = cat["online.icon_ngc4008"](zoom=7).to_dask()
 
-assert (ds["tas"].isel(time=-1).values == ds2["tas"].isel(time=-1).values).all()
+    assert (ds["tas"].isel(time=-1).values == ds2["tas"].isel(time=-1).values).all()
